16f1e78878424c6fff81398874e8ba0affa1fa2b
Artem <artemis.zaytsev@gmail.com>
2023-10-29 23:37:31 +0300

    Initial

---
+++ 3822B1PR1/Zaitsev/sample/MatrixMultComparison.cpp
@@ -0,0 +1,77 @@
+﻿#include "FlatMatrix.h"
+#include "CRSMatrix.h"
+#include <random>
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+int** build(int n, bool is_sparse, int frequency = 10) {
+	int** result = new int*[n];
+	for (size_t i = 0; i < n; i++) result[i] = new int[n] {};
+	for (size_t i = 0; i < n; i++)
+		for (size_t j = 0; j < n; j++)
+			if (!is_sparse)
+				result[i][j] = rand() % 9 + 1;
+			else if (rand() % 100 < frequency + 1) result[i][j] = rand() % 10 + 1;
+	return result;
+}
+
+double test_CRSMatrix(int tests, int size, int frequency) {
+	double time = 0.0;
+	for (size_t i = 0; i < tests; i++) {
+		int** matrix1 = build(size, true, frequency);
+		int** matrix2 = build(size, true, frequency);
+		CRSMatrix A(size, matrix1), B(size, matrix2);
+		auto start = chrono::steady_clock::now();
+		A* B;
+		auto end = chrono::steady_clock::now();
+		auto diff = end - start; 
+		time += chrono::duration<double, milli>(diff).count();
+	}
+	return time / tests;
+}
+
+int main(int argc, char* argv[]) {
+	for (auto size : {500, 700, 1000, 2000, 3000, 4000}) {
+		for (int frequency : {1}) {
+			cout << "size: " << size << endl;
+			cout << "Takes: " << test_CRSMatrix(1, size, frequency) << " ms";
+			cout << endl << endl << endl;
+		}
+	}
+
+	/*int sizes[] {100, 200, 300, 400};
+	int frequencies[] {5, 10, 50, 100};
+
+
+	for (bool is_sparse : {false, true}) {
+		for (int size : sizes) {
+			for (int frequency : frequencies) {
+				int** matrix1 = build(size, is_sparse, frequency);
+				int** matrix2 = build(size, is_sparse, frequency);
+				FlatMatrix A(size, matrix1), B(size, matrix2);
+				auto start = chrono::steady_clock::now();
+				A* B;
+				auto end = chrono::steady_clock::now();
+				auto diff = end - start;
+				cout << "FlatMatrices Multiplication for" << endl << "- size of " << size << endl << "- ";
+				cout << (is_sparse ? "sparse" : "dense") << endl;
+				if (is_sparse) cout << "- frequency is " << frequency << "%" << endl;
+				cout << endl << "Takes " << chrono::duration<double, milli>(diff).count() << " ms";
+				cout << endl << endl << endl;
+
+				CRSMatrix A2(size, matrix1), B2(size, matrix2);
+				start = chrono::steady_clock::now();
+				A2 * B2;
+				end = chrono::steady_clock::now();
+				diff = end - start;
+				cout << "CRSMatrix Multiplication for" << endl << "- size of " << size << endl << "- ";
+				cout << (is_sparse ? "sparse" : "dense") << endl;
+				if (is_sparse) cout << "- frequency is " << frequency << "%" << endl;
+				cout << endl << "Takes " << chrono::duration<double, milli>(diff).count() << " ms";
+				cout << endl << endl << endl;
+				if (!is_sparse) break;
+			}
+		}
+	}*/
+}
---
+++ 3822B1PR1/Zaitsev/source/CRSMatrix.cpp
@@ -0,0 +1,118 @@
+#include "CRSMatrix.h"
+
+CRSMatrix::CRSMatrix() : N(0), NZ(0) {}
+
+CRSMatrix::CRSMatrix(size_t _N, int** matrix) : N(_N), NZ(0) { // не тот конструктор 
+	int row = 0;
+	row_ind.push_back(0);
+	for (size_t i = 0; i < N; i++) {
+		for (size_t j = 0; j < N; j++) {
+			if (matrix[i][j] != 0) {
+				values.push_back(matrix[i][j]);
+				cols.push_back(j);
+				row++;
+			}
+		}
+		row_ind.push_back(row);
+	}
+	NZ = values.size();
+}
+
+CRSMatrix::CRSMatrix(size_t _N, size_t _NZ, vector<int> _values, vector<int> _cols, vector<int> _row_ind):
+	N(_N),
+	NZ(_NZ),
+	values(_values),
+	cols(_cols),
+	row_ind(_row_ind)
+{}
+
+CRSMatrix::CRSMatrix(const CRSMatrix& other) : N(other.N), NZ(other.NZ) {
+	values = vector<int>(other.values);
+	cols = vector<int>(other.cols);
+	row_ind = vector<int>(other.row_ind);
+}
+
+CRSMatrix::~CRSMatrix() {}
+
+CRSMatrix& CRSMatrix::operator=(const CRSMatrix& other) {
+	N = other.N;
+	NZ = other.NZ;
+	values = vector<int>(other.values);
+	cols = vector<int>(other.cols);
+	row_ind = vector<int>(other.row_ind);
+	return *this;
+}
+
+
+CRSMatrix CRSMatrix::operator~() const {
+	vector<int> nvalues(values);
+	vector<int> ncols(cols);
+	vector<int> nrow_ind;
+	for (size_t i = 0; i < N + 1; i++) nrow_ind.push_back(0);
+	for (size_t i = 0; i < NZ; i++) nrow_ind[cols[i] + 1]++;
+	int s = 0;
+	int tmp;
+	for (size_t i = 1; i <= N; i++) {
+		tmp = nrow_ind[i];
+		nrow_ind[i] = s;
+		s += tmp;
+	}
+	int j1, j2, col, ri, li;
+	for (size_t i = 0; i < N; i++) {
+		j1 = row_ind[i];
+		j2 = row_ind[i+1];
+		col = i;
+		for (int j = j1; j < j2; j++) {
+			ri = cols[j];
+			li = nrow_ind[ri + 1];
+			nvalues[li] = values[j];
+			ncols[li] = col;
+			nrow_ind[ri + 1]++;
+		}
+	}
+	return CRSMatrix(N, NZ, nvalues, ncols, nrow_ind);
+}
+
+CRSMatrix CRSMatrix::operator*(const CRSMatrix& o) {
+	CRSMatrix result, other = ~o;
+	result.N = N;
+	result.row_ind.push_back(0);
+
+	for (size_t i = 0; i < N; i++) {
+		for (size_t j = 0; j < N; j++) {
+			int amount = 0;
+			int ks = row_ind[i];
+			int ls = other.row_ind[j];
+			int kf = row_ind[i + 1] - 1;
+			int lf = other.row_ind[j + 1] - 1;
+			while ((ks <= kf) && (ls <= lf)) {
+				if (cols[ks] < other.cols[ls]) ks++;
+				else
+					if (cols[ks] > other.cols[ls]) ls++;
+					else {
+						amount += values[ks] * other.values[ls];
+						ks++; ls++;
+					}
+			}
+			if (amount > 0) {
+				result.cols.push_back(j);
+				result.values.push_back(amount);
+				result.NZ++;
+			}
+		}
+		result.row_ind.push_back(NZ);
+	}
+	return result;
+
+}
+
+ostream& operator<<(ostream& out, const CRSMatrix & v) {
+	out << "Values = { ";
+	for (auto elem : v.values) out << elem << " ";
+	out << "}" << endl << "Cols = { ";
+	for (auto elem : v.cols) out << elem << " ";
+	out << "}" << endl << "RowInds = { ";
+	for (auto elem : v.row_ind) out << elem << " ";
+	out << "}";
+	return out;
+}
